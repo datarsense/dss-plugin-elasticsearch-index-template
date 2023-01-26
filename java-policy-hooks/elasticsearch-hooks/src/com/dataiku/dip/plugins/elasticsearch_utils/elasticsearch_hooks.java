57cc9bfd2ba84c4588592a4f24ec3832511e6fb5
matt6697 <32440697+matt6697@users.noreply.github.com>
2023-01-26 18:50:24 +0100

    Plugin metadata cleanup & apply hook only on specified connections

--- java-policy-hooks/elasticsearch-hooks/src/com/dataiku/dip/plugins/elasticsearch_utils/elasticsearch_hooks.java
+++ java-policy-hooks/elasticsearch-hooks/src/com/dataiku/dip/plugins/elasticsearch_utils/elasticsearch_hooks.java
@@ -15,6 +15,9 @@ import com.dataiku.dip.server.services.TaggableObjectsService.TaggableObject;
 import com.dataiku.dip.server.datasets.DatasetSaveService.DatasetCreationContext;
 import com.dataiku.dip.utils.DKULogger;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import org.json.JSONObject;
 
 
@@ -29,15 +32,21 @@ public class elasticsearch_hooks extends CustomPolicyHooks {
         if(after.getClass() == SerializedDataset.class && this.isElasticSearchDataset((SerializedDataset)after)) {
             // Get a list of columns defined in an ES index template
             JsonObject pluginSettings = regularPluginsRegistryService.getSettings("elasticsearch-index-template").config;
+
+            boolean hasConnectionKey = pluginSettings != null && pluginSettings.has("es-connections");
+            JsonArray connections = hasConnectionKey ? pluginSettings.get("es-connections").getAsJsonArray() : null;
+
             boolean hasColumnsKey = pluginSettings != null && pluginSettings.has("columns");
             String columns = hasColumnsKey ? pluginSettings.get("columns").getAsString() : null;
             String[] columnsList = columns.split(",");
 
+            SerializedDataset ds = (SerializedDataset)after;
+            JsonElement dsConnectionElt = new JsonParser().parse(ds.getParams().getConnection());
+
             // If the list contains at least one column name check if dataset schema contains columns mapped in the ES index template
-            if (columnsList.length > 0) {
-                SerializedDataset ds = (SerializedDataset)after;
+            if (connections.contains(dsConnectionElt.getAsJsonPrimitive()) && columnsList.length > 0) {
+                
                 JSONObject inferedDatasetEsMappingProperties = ElasticSearchUtils.getDefaultMappingDefinition(Dataset.fromSerialized(ds), ElasticSearchDialect.ES_7).getJSONObject("properties");
-                logger.info((Object)("DATASET INFERED MAPPING PROPERTIES " + inferedDatasetEsMappingProperties));
                 
                 // A custom ES mapping will only be required if the dataset contains columns mapped in an index template.
                 boolean customEsMappingRequired = false;
@@ -56,7 +65,7 @@ public class elasticsearch_hooks extends CustomPolicyHooks {
                     JSONObject customDatasetEsMapping = new JSONObject();
                     customDatasetEsMapping.put("properties", inferedDatasetEsMappingProperties);
                     ds.getParamsAs(ElasticSearchDatasetHandler.Config.class).customMapping = customDatasetEsMapping.toString(0);
-                    logger.info((Object)("WRITE CUSTOM MAPPING " + ((ElasticSearchDatasetHandler.Config)ds.getParams()).customMapping));
+                    logger.info((Object)("Configuring custom elasticsearch mapping " + ((ElasticSearchDatasetHandler.Config)ds.getParams()).customMapping) + " for dataset " + ds.name);
                 }
             }
         }
